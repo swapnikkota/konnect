dee2901946a4bb085e5f9f30e7488b2e80fdd12a
swapnikkota <swapnik.kota@gmail.com>
2015-07-15 23:19:36 +0800

    added geo location for postal code search
    added geo location for postal code search

--- client/views/view-group/postalsearch.js
+++ client/views/view-group/postalsearch.js
@@ -1,25 +1,27 @@
-
-Template.postalsearch.helpers({
-
-});
-
-
-
-
-
 Template.postalsearch.onRendered(function() {
   	this.autorun(function () {
     if (GoogleMaps.loaded()) {		     		 
-		$("#postalCode").geocomplete({ details: "form" });
-		//$.fn.geocomplete("#postalCode");		
+		$("#postalCode").geocomplete({ 
+			details: ".details",
+		    detailsAttribute: "data-geo" })
+			.bind("geocode:result", function(event, result){
+				var data = {};
+				console.log(result);
+				 $.each(result.address_components, function(index, object){
+					var name = object.types[0];
+
+					$.each(object.types, function(index, name){
+					  data[name] = object.long_name;
+					  data[name + "_short"] = object.short_name;
+					});
+				  });
+				  console.log(data);
+			});	
+
 	 }
   });
 });
 
-Template.postalsearch.onCreated(function() {
-
-
-});
 
 var searchPostalCode = function(event,template){
 	  event.preventDefault();
