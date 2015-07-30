2408d38df8e781057e8b0ac2e6f9e43f48df62a1
swapnikkota <swapnik.kota@gmail.com>
2015-07-30 23:16:20 +0800

    change for new stylings
    change for new stylings ( creative template)

--- server/_settings.js
+++ server/_settings.js
@@ -6,8 +6,10 @@ if (typeof Meteor.settings === 'undefined')
 
 _.defaults(Meteor.settings, {
   facebook : {
-        clientId: "671930892938681",
-        secret: "b546b4a01772f340396c4cc34a1be755"
+        //clientId: "671930892938681",
+        //secret: "b546b4a01772f340396c4cc34a1be755"
+		clientId : "1676557749228882",
+		secret : "40467c11dc4bb913c4ac895273875b6a"
    }
 });
 
